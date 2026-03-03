4ddec67d4ea386ed7bc3b8dcaf375a073bcb5847
Pablo Cuecas <205069416+CuecasPablo@users.noreply.github.com>
2026-03-03 19:13:21 +0100

    Actualizar mensaje en HolaDesarrolladoresWeb.java

--- HolaDesarrolladoresWeb.java
+++ HolaDesarrolladoresWeb.java
@@ -1,2 +1,1 @@
-public class HolaDesarrolladoresWeb { public static void main(String[] args) {
-System.out.println("Hola, desarrolladores de GitHub!"); }}
+public class HolaDesarrolladoresWeb {\n public static void main(String[] args) {\n System.out.println("Hola, nueva funcionalidad en desarrollo!");\n }\n}
